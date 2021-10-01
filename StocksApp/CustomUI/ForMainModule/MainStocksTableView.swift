1213f752bb616c6712d49932275d7bc99d3a12d6
SavvaShuliatev <71968903+SavvaShuliatev@users.noreply.github.com>
2021-10-01 21:57:19 +0300

    Setup main app settings

---
+++ StocksApp/CustomUI/ForMainModule/MainStocksTableView.swift
@@ -0,0 +1,8 @@
+//
+//  MainStocksTableView.swift
+//  StocksApp
+//
+//  Created by Савва Шулятьев on 01.10.2021.
+//
+
+import UIKit
---
+++ StocksApp/Modules/MainModule/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewController.swift
+//  StocksApp
+//
+//  Created by Савва Шулятьев on 01.10.2021.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupUI() {
+        
+    }
+
+}
